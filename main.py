4c8c4ce68ff4834afbbc3670371f252ed227ac48
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-04-03 20:41:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import hashlib
-import Lib.hmac
-import requests #you need to install using pip install requests
-import time
-
-BASE_URL = 'https://api.dovewallet.com/v1.1'
-method = "/account/getbalances" #example of get balances
-publicKey = "PUBLICKEY"
-secretKey = "SECRETKEY"
-url = BASE_URL + method
-now = int(round(time.time() * 1000))
-query = f"apikey=PUBLICKEY&nonce={now}"  #example of get balances
-apisign = Lib.hmac.new(secretKey.encode(),(url+"?"+query).encode(), hashlib.sha512).hexdigest()
-r = requests.get(url+"?"+query, headers = {"apisign": apisign})
-print(r.text)
+def dovewallet_request(method,option,nonce=True):  
+    import hashlib
+    import Lib.hmac
+    import requests
+    import time
+    BASE_URL = 'https://api.dovewallet.com/v1.1'
+    publicKey = "PUBLICKEY"
+    secretKey = "SECRETKEY"
+    url = BASE_URL + method
+    now = int(round(time.time() * 1000))
+    query = f"apikey={publicKey}" 
+    for i in option.items():
+        query = query + "&" + i[0] + "=" + i[1]
+    if nonce:
+        query = query + "&" + f"nonce={now}"
+    apisign = Lib.hmac.new(secretKey.encode(),(url+"?"+query).encode(), hashlib.sha512).hexdigest()
+    r = requests.get(url+"?"+query, headers = {"apisign": apisign})
+    return r.text
 
 #Donations for original project: 35o179Nd4ZbhVJcRkGAeBe2vMpFKFi4zr9 (bitcoin)
